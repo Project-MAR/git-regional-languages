8a882e3e21e9b71aca9b96e6bc7d2980af321f61
starcolon <tao@starcolon.com>
2016-08-02 21:51:19 +0700

    Metrics : Added initial

---
+++ spark/src/main/scala/metrics.scala
@@ -0,0 +1,21 @@
+package gitlang
+
+/**
+ * Measurement metrics for Spatial distribution
+ */
+object Metrics {
+  
+  def arealSparsity(a: LanguageHistogram, b: LanguageHistogram): Boolean = {
+
+  }
+
+  def arealDensity(a: LanguageHistogram, b: LanguageHistogram): Boolean = {
+
+  }
+
+  def positionLikelihood(a: LanguageHistogram, b: LanguageHistogram): Boolean = {
+
+  }
+
+
+}
